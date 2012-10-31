4c3deb822f8eb19321438683230b1d735f5462a4
Lindsey Kuper <lindsey@rockstargirl.org>
2012-10-31 05:12:09 -0400

    Updated some comments.

--- rwh-ch26/BloomFilter/Mutable.hs
+++ rwh-ch26/BloomFilter/Mutable.hs
@@ -23,8 +23,14 @@ new :: (a -> [Word32]) -> Word32 -> ST s (MutBloom s a)
 -- hash function and an array.  For the hash function, we just pass
 -- along the one we were given.
 
--- I'm not exactly sure what's going on here.  newArray is defined in
--- Data.Array.MArray; it's a method provided by the MArray interface.
--- I'm not sure what is being "lift"ed here.  Do we have to lift the
--- result of newArray into the ST monad?  Or are we lifting "hash"?
+-- newArray is defined in Data.Array.MArray; it's a method provided by
+-- the MArray interface.  But it's a monadic value because newArray
+-- returns a monad-wrapped thing.  We want to apply 'MB hash' (which
+-- is the function that we get from applying 'MB' to 'hash', and which
+-- is still waiting to get an array) to the thing inside the monad.
+-- That's what 'liftM' does here: it "lifts" 'MB hash' to the level of
+-- the monad, so that it can be applied to the thing inside the monad.
+-- The result is the value of type MB, which is what we wanted -- but
+-- it's still inside the monad, which is good because the type of
+-- 'new' calls for something that is wrapped in ST.
 new hash numBits = MB hash `liftM` newArray (0,numBits-1) False
