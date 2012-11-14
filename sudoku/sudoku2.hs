90d262a96fae6d0e96b12a9399bcc09897ed7b81
Lindsey Kuper <lindsey@rockstargirl.org>
2012-11-14 10:39:40 -0500

    Add some comments explaining how the Eval monad works.

--- sudoku/sudoku2.hs
+++ sudoku/sudoku2.hs
@@ -9,14 +9,34 @@ main = do
     [f] <- getArgs
     grids <- fmap lines $ readFile f
 
+    -- Divide the list of sudoku problems to solve into two equal (or
+    -- almost-equal) parts.
     let (as,bs) = splitAt (length grids `div` 2) grids
 
+    -- 'evaluate' will evaluate its argument to WHNF.  (I think we
+    -- need to force this evaluation because, since we're not doing
+    -- anything with the result, GHC would just refrain from doing it
+    -- otherwise?)
+        
+    -- Eval is the monad within which rpar and rseq return their
+    -- results.  runEval runs the Eval computation.
     evaluate $ runEval $ do
-       a <- rpar (deep (map solve as))
-       b <- rpar (deep (map solve bs))
-       rseq a
-       rseq b
-       return ()
+      -- This says that (deep (map solve as)) can run at the same time
+      -- as something else (i.e., can be parallelized).
+      a <- rpar (deep (map solve as))
+      
+      -- (deep (map solve bs)) can also be run at the same time as
+      -- something else.
+      b <- rpar (deep (map solve bs))
+      
+      -- Now fire off the two computations.
+      rseq a
+      rseq b
+      
+      return ()
 
+-- deep evaluates its argument to normal form and returns it.  We need
+-- to do this to ensure that our two subtasks are each accomplishing
+-- something.  In fact, the Par monad has this behavior by default.
 deep :: NFData a => a -> a
 deep a = deepseq a a
