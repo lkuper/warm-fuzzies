a6e199c5ec1e0209f168adb161747ed979df5c71
Lindsey Kuper <lindsey@rockstargirl.org>
2012-11-14 10:53:16 -0500

    Comments about separating out a sequential part of the program.

--- sudoku/sudoku4.hs
+++ sudoku/sudoku4.hs
@@ -9,11 +9,17 @@ main :: IO ()
 main = do
     [f] <- getArgs
     grids <- fmap lines $ readFile f
+    
+    -- We don't actually need the length of grids; this is just a
+    -- trick to force grids to be evaluated entirely before we start
+    -- the parallel part of our program.  It's not necessary, but it
+    -- lets us divide up the sequential and parallel parts.
     evaluate (length grids)
+    
     evaluate $ deep $ runEval $ parMap solve grids
---   better: evaluate $ Seq.seqList Seq.rseq $ runEval $ parMap solve grids
---   better: evaluate $ deep $ (map solve grids `using` parList rwhnf)
---   better: mapM_ evaluate (map solve grids `using` parList rwhnf)
+    --   better: evaluate $ Seq.seqList Seq.rseq $ runEval $ parMap solve grids
+    --   better: evaluate $ deep $ (map solve grids `using` parList rwhnf)
+    --   better: mapM_ evaluate (map solve grids `using` parList rwhnf)
     return ()
 
 parMap :: (a -> b) -> [a] -> Eval [b]
