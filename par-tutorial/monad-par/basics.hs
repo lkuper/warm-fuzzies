05f2f8a41ebf21d9b2ee4dda0a659fbcd01c6f44
Lindsey Kuper <lindsey@rockstargirl.org>
2012-11-20 20:40:38 -0500

    A bit of rearrangement and cleanup.

--- par-tutorial/monad-par/basics.hs
+++ par-tutorial/monad-par/basics.hs
@@ -93,7 +93,7 @@ fact n = n * (fact (n - 1))
 main :: IO ()
 main = do
   print $ runPar $ do
-    factorials <- parMap fact [3, 4, 5, 6, 7, 8, 9, 10]
+    factorials <- parMap fact [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
     return factorials
   return ()
 
